6ac6096c843c02fbba1a2d0e30e5a8dcfbc8a3ff
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-08-26 18:40:05 +0800

    header topbar changes

--- 02-Custom-Real Estate-Homepage/script.js
+++ 02-Custom-Real Estate-Homepage/script.js
@@ -1,7 +1,6 @@
 // <<--------- Header topbar populate dropdown with states --------->>
 
 //--- Array of states --------
-
 const statesList = [
     "Johor",
     "Kedah",
@@ -22,11 +21,11 @@ const statesList = [
 
 //-- Global varibale declartions
 
-const statesOptionList = document.getElementById('listOfStates');  
+const statesOptionList = document.getElementById('listOfStates');
 
-statesList.forEach(state =>{
-    let stateList  = new Option(state);
-    statesOptionList.append(stateList);       
+statesList.forEach(state => {
+    let stateList = new Option(state);
+    statesOptionList.append(stateList);
 });
 
 
@@ -34,9 +33,9 @@ getSelectedDataUI();
 
 //--- Capture Selected option item & setItme in to localstrogre
 
-const getSelectedStateValues = (e) =>{
-   let stateValue = e.target.value;
-   localStorage.setItem("States", JSON.stringify(stateValue));   
+const getSelectedStateValues = (e) => {
+    let stateValue = e.target.value;
+    localStorage.setItem("States", JSON.stringify(stateValue));
 }
 
 //-- Event Listeners
@@ -45,10 +44,30 @@ statesOptionList.addEventListener('change', getSelectedStateValues);
 
 //-- Get data from localstorage and populate into the DOM UI
 
-function getSelectedDataUI(){    
-  const selectedState = JSON.parse(localStorage.getItem('States', statesOptionList));
-  if(selectedState !== null && selectedState.length > 0){   
-    let setOptionsData =  statesOptionList.options[statesOptionList.selectedIndex];
-     setOptionsData.textContent = selectedState;
-  }
+function getSelectedDataUI() {
+    const selectedState = JSON.parse(localStorage.getItem('States', statesOptionList));
+    if (selectedState !== null && selectedState.length > 0) {
+        let setOptionsData = statesOptionList.options[statesOptionList.selectedIndex];
+        setOptionsData.textContent = selectedState;
+    }
 }
+
+// <<--------- Header topbar language translation Items JSON Arr.. -------------->>
+
+const langTranslateItems = {
+    "langTransList": [{
+        "translteIco": "<i class='fa-solid fa-globe'></i>",
+        "en": "EN",
+        "bm": "BM"
+    }],
+};
+
+// ---- Elements --------------------------------
+langTranslateItems.langTransList.forEach(item => {
+    const langTranslationList = document.getElementById('langTranslationsItems');
+    langTranslationList.innerHTML += `
+    <li><a href="#">${item.translteIco}</a></li>
+    <li><a href="#">${item.en}</a></li>
+    <li><a href="#">${item.bm}</a></li>
+  `
+});
