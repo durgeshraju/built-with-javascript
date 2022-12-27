49d96b7ee10b7594fc655669f14c18d2ea4a18ff
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-12-27 17:25:06 +0800

    modalDialog

--- 03.Modal-Dialog/popup.js
+++ 03.Modal-Dialog/popup.js
@@ -1,3 +1,43 @@
 'use strict';
 
 //-------- Grab DOM Elements --------
+
+const btnModalTrigger = document.querySelector("button.btn");
+const modalWrapper = document.querySelector(".modal");
+const modalbtnClose = document.querySelectorAll(".isBtnDismiss");
+const modalCloseBtnArr = Array.from(modalbtnClose);
+
+//-------- Modal Toggle & Overlay --------
+
+const modalVisible = (toggleClass) => {
+   return modalWrapper.classList.toggle(toggleClass);
+};
+
+const modalOverlay = (isOverlay) => {
+    return modalWrapper.classList.toggle(isOverlay);
+};
+
+//-------- Event Listners --------
+
+//--->> Open Modal
+btnModalTrigger.addEventListener('click', () =>{
+  if(modalVisible('d-block')){
+    modalOverlay('opened')    
+  }
+});
+
+//--->> Close Modal
+modalCloseBtnArr.forEach(close => {
+    close.addEventListener('click', () =>{
+        modalVisible('d-block');
+        modalOverlay('opened');
+    });
+});
+
+//--->> Keybaord Event `ESC` key close modal
+document.addEventListener('keydown', (event) =>{
+    if(event.key === "Escape"){
+        modalVisible('d-block');
+        modalOverlay('opened');
+    }
+});
