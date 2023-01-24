eaba63ab0048017a4c03686fa84ed028c1ab699e
ndurgesh88 <durgeshnidadavolu@gmail.com>
2023-01-24 18:02:17 +0800

    localstorage getItem Display UI

--- 08-MovieSeatBooking/script.js
+++ 08-MovieSeatBooking/script.js
@@ -1,12 +1,29 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.seat');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+// Get data from localstorage and populate UI
+const populateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('SelectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+populateUI();
 
 let ticketPrice = +movieSelect.value;
-// Get data from localstorage and populate UI
 
 // Save selected movie index and price
 const setMovieData = (movieIndex, moviePrice) => {
@@ -24,20 +41,21 @@ const updateSelectedSeatCount = () => {
     total.innerHTML = seatSelectedCount * ticketPrice;
 }
 
-
 //----------------- Event Listeners ----------------
-
-//const result = seats.filter (seat => !seat.classList.contains("occupied"));
 
 // Pick select movie event
 
-movieSelect.addEventListener('change', (e) => {    
-    ticketPrice = +e.target.value;
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;    
     setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedSeatCount();
-})
-
-container.addEventListener('click', (e) =>{    
-    !e.target.classList.contains('occupied') && e.target.classList.toggle('selected');
-    updateSelectedSeatCount();
 });
+
+container.addEventListener('click', (e) =>{
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+      e.target.classList.toggle('selected');
+      updateSelectedSeatCount();
+  }
+});
+
+updateSelectedSeatCount();
